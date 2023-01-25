b26a43f9e0275e3c45993fabbabcd4e4106cca2f
Franco <francomanca93@gmail.com>
2023-01-25 16:09:43 +0100

    util config and restructuring methods in spider class

--- Notes/notes.md
+++ Notes/notes.md
@@ -821,72 +821,34 @@ scrapy crawl qoutes -a top=3
 Esta clase explora mas configuraciones de custom settings de la clase scrapy.spider
 
 ```py
-
-class QoutesSpider(scrapy.Spider):
-    name = 'quotes'
-    start_urls = [
-        'http://quotes.toscrape.com'
-    ]
-
+...
     # uso custom settings para archivo
     custom_settings = {
         #guardado de archivos
-        'FEED_URI': 'quotes.json',
-        'FEED_FORMAT': 'json',
+        'FEEDS': {
+            'quotes.json': {
+                'format': 'json',
+                # Cambiar el encoding del archivo
+                'encoding': 'utf8',
+                'store_empty': False,
+                'fields': None,
+                'indent': 4,
+                'item_export_kwargs': {
+                    'export_empty_fields': True,
+                },
+            },
+        },
         # numero maximo peticiones asincronas 
         'CONCURRENT_REQUEST': 24,
         # maximo de ram
         'MEMUSAGE_LIMIT_MB': 2048,
-        'MEMUSAGE_NOTIFY_MAIL': ['rb@rb.com'],
+        'MEMUSAGE_NOTIFY_MAIL': ['fm@fm.com'],
         # No violar pautas de robots.txt
         'ROBOTSTXT_OBEY': True,
         # custom User Agent
-        'USER_AGENT': 'ElmerHomero',
-        # Cambiar el encoding del archivo
-        'FEED_EXPORT_ENCODING': ' utf-8'
+        'USER_AGENT': 'Peter Parker',
     }
-
- def parse_only_quotes(self, response, **kwargs):
-        if kwargs:
-            quotes = kwargs['quotes']
-        quotes.extend(response.xpath(
-            '//span[@class="text" and @itemprop="text"]/text()').getall())
-
-        next_page_button_link = response.xpath(
-            '//ul[@class="pager"]//li[@class="next"]/a/@href').get()
-        if next_page_button_link:
-            yield response.follow(next_page_button_link,
-                                  callback=self.parse_only_quotes,
-                                  cb_kwargs={'quotes': quotes})
-        else:
-            yield {
-                'quotes': quotes,
-            }
-
-    def parse(self, response):
-        title = response.xpath('//h1/a/text()').get()
-        quotes = response.xpath(
-            '//span[@class="text" and @itemprop="text"]/text()').getall()
-        top_tags = response.xpath(
-            '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
-
-        top = getattr(self, 'top', None)
-        if top:
-            top = int(top)
-            top_tags
-
-        yield {
-            'title': title,
-            'top_tags': top_tags[:top]
-        }
-
-        next_page_button_link = response.xpath(
-            '//ul[@class="pager"]//li[@class="next"]/a/@href').get()
-        if next_page_button_link:
-            yield response.follow(next_page_button_link,
-                                  callback=self.parse_only_quotes,
-                                  cb_kwargs={'quotes': quotes})
-
+...
 ```
 
 Volvemos a ejecutar el crawl
@@ -908,14 +870,23 @@ class QuotesSpider(scrapy.Spider):
         'http://quotes.toscrape.com'
     ]
     custom_settings = {
-        'FEED_URI': 'quotes.json',
-        'FEED_FORMAT': 'json',
-        'CURRENT_REQUESTS': 24,
+        'FEEDS': {
+            'quotes.json': {
+                'format': 'json',
+                'encoding': 'utf8',
+                'store_empty': False,
+                'fields': None,
+                'indent': 4,
+                'item_export_kwargs': {
+                    'export_empty_fields': True,
+                },
+            },
+        },
+        'CONCURRENT_REQUESTS': 24,
         'MEMUSAGE_LIMIT_MB': 2048,
-        'MEMUSAGE_NOTIFY_MAIL': ['rb@rb.com'],
+        'MEMUSAGE_NOTIFY_MAIL': ['fm@fm.com'],
         'ROBOTSTXT_OBEY': True,
-        'USER_AGENT': 'ElherHomero',
-        'FEED_EXPORT_ENCODING': 'utf-8'
+        'USER_AGENT': ''
     }
 
     def parse_only_quotes(self, response, **kwargs):
--- quotes_scraper/quotes_scraper/spiders/quotes_scraper.py
+++ quotes_scraper/quotes_scraper/spiders/quotes_scraper.py
@@ -19,54 +19,72 @@ class QuotesSpider(scrapy.Spider):
                 },
             },
         },
+        'CONCURRENT_REQUESTS': 24,
+        'MEMUSAGE_LIMIT_MB': 2048,
+        'MEMUSAGE_NOTIFY_MAIL': ['fm@fm.com'],
+        'ROBOTSTXT_OBEY': True,
+        'USER_AGENT': ''
     }
 
     def parse_only_quotes(self, response, **kwargs):
         if kwargs:
-            quotes = kwargs['quotes']
-        quotes_path = '//span[@class="text" and @itemprop="text"]/text()'
-        quotes.extend(response.xpath(quotes_path).getall())
-        
-        next_page_button_path = '//ul[@class="pager"]//li[@class="next"]/a/@href'
-        next_page_button_link = response.xpath(next_page_button_path).get()
-        if next_page_button_link:
-            yield response.follow(
-                next_page_button_link, 
-                callback=self.parse_only_quotes, 
-                cb_kwargs={'quotes':quotes}
-            )
-        else:
-            yield {
-                'quotes': quotes
-            }
+            # Generate new Quotes (Page)
+            new_quotes = self._get_all_author_quotes(response)
+            kwargs["quotes"].extend(new_quotes)
+            # Get the new link
+            next_page = self._get_next_link(response)
+            if next_page:
+                yield response.follow(
+                    next_page,
+                    callback=self.parse_only_quotes,
+                    cb_kwargs=kwargs
+                )
+            else:
+                yield kwargs
 
     def parse(self, response):
         """ it help us to analize the file and extract information from this"""
-        
-        title_path = '//h1/a/text()'
-        title = response.xpath(title_path).get()
+        title = self._get_title(response)
+        quotes = self._get_all_author_quotes(response)
+        top_tags = self._get_top_tags(response)
 
-        quotes_path = '//span[@class="text" and @itemprop="text"]/text()'
-        quotes = response.xpath(quotes_path).getall()
+        # Get the new link
+        next_page = self._get_next_link(response)
+        if next_page:
+            yield response.follow(
+                next_page,
+                callback=self.parse_only_quotes,
+                cb_kwargs={
+                    "title": title, 
+                    "top_tags": top_tags,
+                    "quotes": quotes
+                }
+        )
 
-        top_tags_path = '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()'
-        top_tags = response.xpath(top_tags_path).getall()
-        
+    # Basic data private methods
+    def _get_title(self, response):
+        return response.xpath('//h1/a/text()').get()
+
+    def _get_all_author_quotes(self, response):
+        quotes = self.__get_all_quotes(response)
+        authors = self.__get_all_author(response)
+        return [({'quote':value_one, 'author': value_two}) for value_one, value_two in zip(quotes, authors)]
+
+    def __get_all_quotes(self, response):
+            return response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+
+    def __get_all_author(self, response):
+        return response.xpath('//span/small[@class="author" and @itemprop="author"]/text()').getall()
+
+    def _get_top_tags(self, response):
+        ''' Generate Content -a [OPTION]\nOption: -a top=3 Get the first 3 of the top '''
+        top_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+        # Add Params
         top = getattr(self, 'top', None)
         if top:
-            top = int(top)
-            top_tags = top_tags[:top]
-
-        yield {
-            'title': title,
-            'top_tags': top_tags
-        }
-        
-        next_page_button_path = '//ul[@class="pager"]//li[@class="next"]/a/@href'
-        next_page_button_link = response.xpath(next_page_button_path).get()
-        if next_page_button_link:
-            yield response.follow(
-                next_page_button_link, 
-                callback=self.parse_only_quotes, 
-                cb_kwargs={'quotes': quotes}
-            )
+            top_tags = top_tags[:int(top)]
+        return top_tags
+    
+    # Get the new link private method
+    def _get_next_link(self, response):
+        return response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
